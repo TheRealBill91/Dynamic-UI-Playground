399fe4fe85139e5364644a99b87a6962517e741d
Billy <bc35786@gmail.com>
2023-03-31 14:57:43 -0500

    Finish drop down menu (basic)

--- src/toggleDropDownMenu.js
+++ src/toggleDropDownMenu.js
@@ -1,15 +1,12 @@
 const toggleDropDownMenu = () => {
-    const button = document.querySelectorAll("button");
-    const ulContainer = document.querySelector("body > div > div");
-    button.forEach((btn) =>
-        btn.addEventListener("click", (event) => {
-            event.currentTarget.nextElementSibling.classList.toggle("visible");
-            /* ulContainer.classList.toggle("visible"); */
-        })
-    );
-    /*  button.addEventListener("click", (event) => {
-           ulContainer.classList.toggle("visible");
-       }); */
+  const button = document.querySelectorAll("button");
+  const ulContainer = document.querySelector("body > div > div");
+  button.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+      event.currentTarget.nextElementSibling.classList.toggle("visible");
+
+    })
+  );
 };
 
 export { toggleDropDownMenu };
